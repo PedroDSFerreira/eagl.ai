9dfbb81ce0d4c1ac4f554eeb7ba61695faedb800
Pedro Ferreira <pedrodsf21@gmail.com>
2024-04-15 18:27:28 +0100

    Initial maven project setup

---
+++ src/main/java/com/pdsf/eagleai/EagleaiApplication.java
@@ -0,0 +1,13 @@
+package com.pdsf.eagleai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdsf/eagleai/EagleaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdsf.eagleai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
