a3143db1667ecb6c0a01802c7a183c3b19bf69f4
Pedro Ferreira <pedrodsf21@gmail.com>
2024-04-18 00:27:42 +0100

    Update contact service logic

--- src/main/java/dev/pdsf/eagleai/service/ContactService.java
+++ src/main/java/dev/pdsf/eagleai/service/ContactService.java
@@ -2,6 +2,7 @@ package dev.pdsf.eagleai.service;
 
 import dev.pdsf.eagleai.exception.ContactNotFoundException;
 import dev.pdsf.eagleai.model.Contact;
+import dev.pdsf.eagleai.model.Description;
 import dev.pdsf.eagleai.repository.ContactRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,11 @@ import java.util.List;
 @Service
 public class ContactService {
     private final ContactRepository repository;
+    private final DescriptionService descriptionService;
 
-    public ContactService(ContactRepository repository) {
+    public ContactService(ContactRepository repository, DescriptionService descriptionService) {
         this.repository = repository;
+        this.descriptionService = descriptionService;
     }
 
     public List<Contact> listContacts() {
@@ -25,19 +28,28 @@ public class ContactService {
     }
 
     public Contact newContact(Contact newContact) {
+        if (newContact.getImageData() != null) {
+            // Call ollama, get reply
+            // Parse reply, get description params
+            // Create new description w/params, and add to contact
+            Description description = new Description(null, null,null, null, null, false);
+            descriptionService.newDescription(description);
+            newContact.setDescription(description);
+        }
         return repository.save(newContact);
     }
 
     public Contact updateContact(Contact newContact, Long id) {
-        return repository.findById(id)
-                .map(contact -> {
-                    contact.setName(newContact.getName());
-                    contact.setEmail(newContact.getEmail());
-                    contact.setPhone(newContact.getPhone());
-                    contact.setAddress(newContact.getAddress());
-                    return repository.save(contact);
+        Contact contact = repository.findById(id)
+                .map(c -> {
+                    c.setName(newContact.getName());
+                    c.setEmail(newContact.getEmail());
+                    c.setPhone(newContact.getPhone());
+                    c.setAddress(newContact.getAddress());
+                    return c;
                 })
                 .orElseThrow(() -> new ContactNotFoundException(id));
+        return repository.save(contact);
     }
 
     public void deleteContact(Long id) {
