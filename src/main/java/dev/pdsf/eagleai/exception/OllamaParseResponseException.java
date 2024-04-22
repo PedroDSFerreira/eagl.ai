ae166d4aab49f0cb63436eccaac16f65a3f51cbf
Pedro Ferreira <pedrodsf21@gmail.com>
2024-04-22 11:35:02 +0100

    Update ollama error message

--- src/main/java/dev/pdsf/eagleai/exception/OllamaParseResponseException.java
+++ src/main/java/dev/pdsf/eagleai/exception/OllamaParseResponseException.java
@@ -6,6 +6,6 @@ import org.springframework.web.server.ResponseStatusException;
 public class OllamaParseResponseException extends RuntimeException {
     public OllamaParseResponseException(String ollamaResponse) {
         throw new ResponseStatusException(
-                HttpStatus.INTERNAL_SERVER_ERROR, "Unable to parse ollama response '" + ollamaResponse.strip() + "'", this);
+                HttpStatus.INTERNAL_SERVER_ERROR, "Unable to parse ollama response: '" + ollamaResponse.strip() + "'", this);
     }
 }
