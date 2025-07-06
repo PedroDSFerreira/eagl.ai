f59dba9200e1fe405ee789ffacf877b80c116503
Pedro Ferreira <pedrodsf@ua.pt>
2025-07-06 10:41:30 +0100

    Fix nested frontend directory structure

---
+++ frontend/eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import * as parserVue from 'vue-eslint-parser'
+import configPrettier from '@vue/eslint-config-prettier'
+import configTypescript from '@vue/eslint-config-typescript'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  ...configTypescript(),
+  configPrettier,
+]
